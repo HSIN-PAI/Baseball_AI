5dab1f88fefc4db493aa8d2f3af6694cbc0bf769
PAI <zz556659@gmail.com>
2023-12-26 21:26:37 +0800

    [Improved] 優化Export功能
    1.自行選擇匯入檔案
    2.自行選擇存檔路徑

--- Export.py
+++ Export.py
@@ -1,5 +1,12 @@
 import csv
 from bs4 import BeautifulSoup
+from tkinter import Tk, filedialog
+
+def open_file_dialog():
+    root = Tk()
+    root.withdraw()  # 隱藏主視窗
+    file_path = filedialog.askopenfilename(title="選擇檔案")
+    return file_path
 
 def parse_player_data(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -27,28 +34,30 @@ def parse_player_data(html_content):
         print("找不到球員數據區塊。")
         return None
 
-def save_to_csv(data, csv_filename):
+def save_to_csv(data):
     if data:
-        with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
+        root = Tk()
+        root.withdraw()  # 隱藏主視窗
+        file_path = filedialog.asksaveasfilename(title="儲存檔案",filetypes=[("CSV 檔案", "*.csv"), ("所有檔案", "*.*")])
+        with open(file_path, 'w', newline='', encoding='utf-8') as csvfile:
             fieldnames = data[0].keys()
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
             # 寫入表頭
             writer.writeheader()
-
             # 寫入數據
             writer.writerows(data)
 
-        print(f"數據已成功保存至 {csv_filename}")
+        print(f"數據已成功保存至 {file_path}")
     else:
         print("無法保存數據，請檢查是否成功解析。")
 
 # 讀取 HTML 檔案
-with open('data_input.xml', 'r', encoding='utf-8') as file:
+selected_file = open_file_dialog()
+with open(selected_file, 'r', encoding='utf-8') as file:
     player_data_html = file.read()
 
 # 解析球員數據
 player_data = parse_player_data(player_data_html)
 
 # 保存為 CSV 檔案
-save_to_csv(player_data, 'player_data.csv')
+save_to_csv(player_data)
